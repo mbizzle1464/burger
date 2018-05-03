91fe6319da42d91ded81f4eafd985807481abad1
Michael Bizzle <mbizzle@studiomoviegrill.com>
2018-05-03 15:32:11 -0500

    Created db folder, schema.sql, and seeds.sql files.

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+-- Create the database burgers_db and specified it for use.
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN NOT NULL,
+    PRIMARY KEY(id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Single Cheeseburger', FALSE);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Double Cheeseburger', FALSE);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Triple Cheeseburger', FALSE);
